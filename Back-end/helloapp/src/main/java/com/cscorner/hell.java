d743203bf0d6bf9361c1f481868f3421f749ec76
MikeMikey2 <trafalgardwhaterlaw87@gmail.com>
2026-07-29 12:15:50 +0100

    first commit

---
+++ Back-end/helloapp/src/main/java/com/cscorner/hell.java
@@ -0,0 +1,7 @@
+package com.cscorner;
+
+public class hell {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ Back-end/helloapp/src/main/java/com/cscorner/hellocontroller.java
@@ -0,0 +1,12 @@
+package com.cscorner;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hellocontroller {
+  @GetMapping("/hello")
+  public String sayHello() {
+    return "Hello world!";
+  }
+}
