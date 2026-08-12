e58180fc1d4999acc51749d1d0db9cda06650070
MikeMikey2 <trafalgardwhaterlaw87@gmail.com>
2026-08-12 16:55:08 +0100

    commit 26

--- Back-end/Backapp/src/main/java/com/cscorner/Role.java
+++ Back-end/Backapp/src/main/java/com/cscorner/Role.java
@@ -1,5 +1,6 @@
 package com.cscorner;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,17 +10,25 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "Role")
 public class Role {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_role") // <-- Fait le lien avec id_role dans MySQL
     private Integer idRole;
 
+    @Column(name = "nom_role") // <-- Fait le lien avec nom_role dans MySQL
     private String nomRole;
+
+    @Column(name = "type_role") // <-- Fait le lien avec type_role dans MySQL
     private String typeRole;
 
+    // Getters & Setters
     public Integer getIdRole() { return idRole; }
     public void setIdRole(Integer id) { this.idRole = id; }
+
     public String getNomRole() { return nomRole; }
     public void setNomRole(String nom) { this.nomRole = nom; }
+
     public String getTypeRole() { return typeRole; }
     public void setTypeRole(String type) { this.typeRole = type; }
-} 
+}
