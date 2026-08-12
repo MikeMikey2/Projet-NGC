3676fd6edc3b4686ba1b4bc0572e4eede7a62edd
MikeMikey2 <trafalgardwhaterlaw87@gmail.com>
2026-08-12 11:49:55 +0100

    commit 20

--- Back-end/Backapp/src/main/java/com/cscorner/User.java
+++ Back-end/Backapp/src/main/java/com/cscorner/User.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackappApplication {
+public class User {
     public static void main(String[] args) {
-        SpringApplication.run(BackappApplication.class, args);
+        SpringApplication.run(User.class, args);
     }
 }
