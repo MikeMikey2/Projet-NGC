80a87a2a14a2fa9c2fed6403b2cb52981d4ee3a5
MikeMikey2 <trafalgardwhaterlaw87@gmail.com>
2026-08-12 15:26:30 +0100

    commit 24

--- Back-end/Backapp/src/main/java/com/cscorner/Utilisateur.java
+++ Back-end/Backapp/src/main/java/com/cscorner/Utilisateur.java
@@ -12,56 +12,69 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-    @Entity
-    @Table(name = "utilisateur")
-    public class Utilisateur {
-        @ManyToMany(fetch = FetchType.EAGER)
+
+@Entity
+@Table(name = "utilisateur")
+public class Utilisateur {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idUtilisateur;
+
+    private String nomUtilisateur;
+    private String prenomUtilisateur;
+    private String email;
+    private String motDePasse;
+
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
-    name = "Utilisateur_Role",
-    joinColumns = @JoinColumn(name = "id_utilisateur"),
-    inverseJoinColumns = @JoinColumn(name = "id_role")
-)
+        name = "Utilisateur_Role",
+        joinColumns = @JoinColumn(name = "id_utilisateur"),
+        inverseJoinColumns = @JoinColumn(name = "id_role")
+    )
+    private Set<Role> roles = new HashSet<>();
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Integer idUtilisateur;
+    // --- Getters et Setters ---
 
-        private String nomUtilisateur;
-        private String prenomUtilisateur;
-        private String email;
-        private String motDePasse;
-        private Set<Role> roles = new HashSet<>();
-
-        public Integer getIdUtilisateur() {
-            return idUtilisateur;
-        }
-        public void setIdUtilisateur(Integer idUtilisateur) {
-            this.idUtilisateur = idUtilisateur;
-        }
-        public String getNomUtilisateur() {
-            return nomUtilisateur;
-        }
-        public void setNomUtilisateur(String nomUtilisateur) {
-            this.nomUtilisateur = nomUtilisateur;
-        }
-        public String getPrenomUtilisateur() {
-            return prenomUtilisateur;
-        }
-        public void setPrenomUtilisateur(String prenomUtilisateur) {
-            this.prenomUtilisateur = prenomUtilisateur;
-        }
-        public String getEmail() {  
-            return email;
-        }
-        public void setEmail(String email) {
-            this.email = email;
-        }
-        public String getMotDePasse() {
-            return motDePasse;
-        }
-        public void setMotDePasse(String motDePasse) {
-            this.motDePasse = motDePasse;
-        }
-        public Set<Role> getRoles() { return roles; }
-        public void setRoles(Set<Role> roles) { this.roles = roles; }
+    public Integer getIdUtilisateur() {
+        return idUtilisateur;
     }
+    public void setIdUtilisateur(Integer idUtilisateur) {
+        this.idUtilisateur = idUtilisateur;
+    }
+
+    public String getNomUtilisateur() {
+        return nomUtilisateur;
+    }
+    public void setNomUtilisateur(String nomUtilisateur) {
+        this.nomUtilisateur = nomUtilisateur;
+    }
+
+    public String getPrenomUtilisateur() {
+        return prenomUtilisateur;
+    }
+    public void setPrenomUtilisateur(String prenomUtilisateur) {
+        this.prenomUtilisateur = prenomUtilisateur;
+    }
+
+    public String getEmail() {  
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
+
+    public Set<Role> getRoles() { 
+        return roles; 
+    }
+    public void setRoles(Set<Role> roles) { 
+        this.roles = roles; 
+    }
+}
